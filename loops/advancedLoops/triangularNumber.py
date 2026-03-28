4848ed5b6e70cb753a955db09b386956e93838a5
PietroLim4 <pietro.lima604@gmail.com>
2026-03-27 23:34:32 -0300

    Loops separation and addition

---
+++ loops/advancedLoops/triangularNumber.py
@@ -0,0 +1,9 @@
+# Check if a number is triangular (product of three consecutive integers)
+n=int(input("Num: "))
+cont=0
+while cont*(cont+1)*(cont+2)<n:
+    cont+=1
+if cont*(cont+1)*(cont+2)==n:
+    print("Triangular")
+else:
+    print("Not triangular")
